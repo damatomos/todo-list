94a3808ff402ae97b2b5292a4b7f2ba81aaa4afb
Mikhael D'Amato <mikhaeldamato@financas.pref>
2023-10-09 09:46:24 -0300

    Initial commit

---
+++ src/test/java/br/com/damatomos/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.damatomos.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
