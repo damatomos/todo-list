1a0aa95d007285375987be57eb8eb14120006edc
damatomos <damato578@gmail.com>
2023-10-12 20:58:44 -0300

    feat: add authentication to task route and insert find and update task

--- src/main/java/br/com/damatomos/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/damatomos/todolist/filter/FilterTaskAuth.java
@@ -16,7 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth extends OncePerRequestFilter{
+public class FilterTaskAuth extends OncePerRequestFilter {
 
   @Autowired
   private IUserRepository userRepository;
@@ -25,8 +25,17 @@ public class FilterTaskAuth extends OncePerRequestFilter{
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
       throws ServletException, IOException {
 
-        // Get Auth Credentials
-        var auth = request.getHeader("Authorization");
+    var servletPath = request.getServletPath();
+
+    if (servletPath.startsWith("/tasks/")) {
+      // Get Auth Credentials
+      var auth = request.getHeader("Authorization");
+      System.out.println("Auth: " + auth);
+      if (auth == null || auth.length() == 0)
+      {
+        response.sendError(401, "No Credentials");
+      }
+      else {
         var base64String = auth.split(" ")[1];
 
         byte[] authDecode = Base64.getDecoder().decode(base64String);
@@ -34,24 +43,30 @@ public class FilterTaskAuth extends OncePerRequestFilter{
         String[] credentials = new String(authDecode).split(":");
         String username = credentials[0];
         String password = credentials[1];
+        System.out.println("Username: " + username);
 
         // Validate User
         var user = this.userRepository.findByUsername(username);
-        if (user == null) response.sendError(401);
-        else
+        if (user == null)
         {
+          response.sendError(401);
+        }
+        else {
           // Validate Password
           var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
-          if (passwordVerify.verified)
-          {
+          if (passwordVerify.verified) {
+            request.setAttribute("userId", user.getId());
             chain.doFilter(request, response);
-          } else
-          {
+          } else {
             response.sendError(401);
           }
         }
+      }
+
+    } else {
+      chain.doFilter(request, response);
+    }
   }
 
-  
 }
--- src/main/java/br/com/damatomos/todolist/task/TaskController.java
+++ src/main/java/br/com/damatomos/todolist/task/TaskController.java
@@ -1,10 +1,22 @@
 package br.com.damatomos.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.damatomos.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,10 +26,43 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel task)
+  public ResponseEntity create(@RequestBody TaskModel task, HttpServletRequest request)
   {
+    UUID userID = (UUID) request.getAttribute("userId");
+    task.setUserID(userID);
+    System.out.println("UserID: " + userID);
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(task.getStartAt()) || currentDate.isAfter(task.getEndAt()))
+    {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+             .body("A data de início / término deve ser maior que a data atual.");
+    }
+
+    if (task.getStartAt().isAfter(task.getEndAt()))
+    {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+             .body("A data de início deve ser menor que a data de término.");
+    }
+
     var taskData = this.taskRepository.save(task);
-    return taskData;
+    return ResponseEntity.status(HttpStatus.CREATED).body(taskData);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> find(HttpServletRequest request)
+  {
+    UUID userID = (UUID) request.getAttribute("userId");
+    return this.taskRepository.findByUserID(userID);
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel task, @PathVariable UUID id, HttpServletRequest request)
+  {
+    var currentTask = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(task, currentTask);
+
+    return this.taskRepository.save(task);
   }
 
 }
